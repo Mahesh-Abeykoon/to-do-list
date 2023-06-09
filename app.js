8c68e9228dea9234067a51173f32180aa5d01306
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-06-09 10:58:25 +0530

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const items = [];
 
 const port = 5050;
 
@@ -8,22 +13,20 @@ app.set('view engine','ejs');
 
 app.get('/', function(req, res){
 
-
-
-    // var options = {
-    //     weekday: "long",
-    //     year: "numeric",
-    //     month: "long"
-    // }
+    var options = { day: "numeric", year: "numeric", month: "long" }
 
     var today = new Date();
+    
     // console.log(today.toLocaleDateString("en-US"));
-
-    // res.send(day)
-    res.render("list", {
-        dayIs: today
+    res.render("list", { dayIs: today, dayItems: items
     });
 });
+
+app.post("/", function(req,res){
+    var item = req.body.newItem
+    items.push(item);
+    res.redirect("/")
+})
 
 app.listen(port, function(){
     console.log("App is Running on port : "+port);
