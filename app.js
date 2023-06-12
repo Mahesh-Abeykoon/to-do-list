ab2d7a687e385660bfed42787cd54096d2a0ab94
Mahesh-Abeykoon <mahesh.a@affiniti.biz>
2023-06-12 12:50:41 +0530

    Updates

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.get('/', function(req, res){
 
 app.post("/", function(req,res){
     let item = req.body.newItem;
-
+    // console.log(req.body)
+ 
     if(req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work")
